02986f935d020972315754fc70f26030e4a19452
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-04-13 12:57:43 +0300

    added id for app-form-input

--- src/components/ui/AppFormInput.vue
+++ src/components/ui/AppFormInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form-control">
-    <label v-if="label" :for="'input-' + Math.random()">{{ label }}</label>
-    <input type="text" :id="'input-' + Math.random()" :value="initialValue" @input="$emit('input', $event)">
+    <label v-if="label" :for="'input-' + id">{{ label }}</label>
+    <input type="text" :id="'input-' + id" :value="initialValue" @input="$emit('input', $event)">
 <!--           @change="onChange" :value="initialValue"-->
   </div>
 </template>
@@ -11,11 +11,14 @@ import {defineComponent, ref} from 'vue'
 export default defineComponent({
   name: "AppFormControl",
   props: ['initialValue', 'label', 'onChange'],
+  emits: ['input'],
   setup(props) {
     const { initialValue } = props
     const value = ref(initialValue)
+    const id = ref(Math.random())
     return {
-      value
+      value,
+      id
     }
   },
 })
